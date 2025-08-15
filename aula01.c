9dd176c189f1a11d7d81dc9f2e5f0db186c0cecf
ricardo-45 <ricardoc.nunes@hotmail.com>
2025-08-15 07:56:09 -0300

    Aula do Dia 15 de Agosto de 2025

---
+++ aula01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nSejam bem-vindos à Universidade Federal de Goiás!\n");
+    printf("Iniciaremos a nossa disciplina Introdução à Programação");
+
+    return 0;
+}
