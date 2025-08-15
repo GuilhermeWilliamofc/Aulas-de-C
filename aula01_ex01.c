ccd6cf7be5b724e0dbbde6e476cfb3aae92e5240
ricardo-45 <ricardoc.nunes@hotmail.com>
2025-08-15 08:23:13 -0300

    Aula do Dia 15 de Agosto de 2025 (Parte 2)

--- aula01_ex01.c
+++ aula01_ex01.c
@@ -3,7 +3,8 @@
 
 int main() /* precisa criar uma função main, aparentemente precisa explicitar o tipo primitivo que vai ser retornado */
 { /* pode abrir chaves tanto quebrando linha quanto do lado dos parênteses */
-    printf("\nOlá, Mundo!");
+    printf("\nSejam bem-vindos à Universidade Federal de Goiás!\n");
+    printf("Iniciaremos a nossa disciplina Introdução à Programação");
     /* aspas simples ('') no printf dá erro */
     /* "f" significa formatado, acho que deve ser semelhante as f-strings */
     /* "/n" significa break row */
---
+++ aula01_ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h> // Biblioteca com comandos de data e hora
+
+int main(){
+    struct tm
+    {
+        int tm_year;
+    };
+
+    /* struct tm: estrutura de dados que armazena valores referentes a data e hora, assim, contendo nove (9) variáveis declaradas dentro deste struct para tal. */
+}
