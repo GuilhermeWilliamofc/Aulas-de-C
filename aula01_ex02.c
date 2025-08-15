3fe170a2ee0121658eb4cc69f385ed053de5373f
ricardo-45 <ricardoc.nunes@hotmail.com>
2025-08-15 08:34:17 -0300

    Aula do Dia 15 de Agosto de 2025 (Parte 3)

--- aula01_ex02.c
+++ aula01_ex02.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <time.h> // Biblioteca com comandos de data e hora
+#include <stdio.h> // Necessária para funções como printf()
+#include <time.h> // Biblioteca com comandos de data e hora, Importa a biblioteca que contém funções para manipular data e hora no C, como time() e localtime()
 
-int main(){
-    struct tm
-    {
-        int tm_year;
-    };
-
+int main(){ // O ponto de entrada do programa. Toda execução em C começa por aqui.
+    time_t segundos = time(NULL); /* time_t acho que seria uma classe da lib */
+    struct tm* tempo_atual=localtime(&segundos);
     /* struct tm: estrutura de dados que armazena valores referentes a data e hora, assim, contendo nove (9) variáveis declaradas dentro deste struct para tal. */
+
+    printf("\nSeja bem-vindo a UFG neste ano de %d", (tempo_atual->tm_year + 1900)); /* %d significa inteiro decimal, parece que tem que fazer um ponteiro para o ano */
+    printf("!"); /* uma pequena concatenação */
+    return 0;
 }
